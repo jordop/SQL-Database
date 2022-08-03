19c12eb8a6c815546dd419481fada8c0c14ac19b
Jordan Powter <jordan.powter@gmail.com>
2022-08-03 11:53:50 +1000

    Update

--- SQL Python.py
+++ SQL Python.py
@@ -1,14 +1,16 @@
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
+
 # The following creates the connection to the MySQL database Server
-def create_server_connection(host_name, user_name, user_password):
+def create_server_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=db_name
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -32,3 +34,45 @@ def create_database(connection, query):
 
 create_database_query = "CREATE DATABASE school"
 create_database(connection, create_database_query)
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+# Assign our SQL command to a python variable using triple quotes to create a multi-line string
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+
+connection = create_db_connection("localhost", "FoxtrotDelta7884*", pw, db) # Connect to the Database
+execute_query(connection, create_teacher_table) # Execute our defined query
+
